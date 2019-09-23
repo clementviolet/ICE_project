6a7766e2c8ddca3358e8b4ad98875ad3117a1d9d
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-09-23 06:24:16 +0200

    Initial commit

---
+++ script.py
@@ -0,0 +1,5 @@
+#%% [markdown]
+#
+## Introduction to Computational Ecology
+# This Jupyter notbebook is the place where all the chapter of Roff's book about
+# invasibility analysis is translated from R to Python.
