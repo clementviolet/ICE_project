835dfd64f38d02b962f6c88b4be00a4fd8db0352
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-09-23 06:36:35 +0200

    Added libraries + first section headings

--- script.py
+++ script.py
@@ -3,3 +3,14 @@
 ## Introduction to Computational Ecology
 # This Jupyter notbebook is the place where all the chapter of Roff's book about
 # invasibility analysis is translated from R to Python.
+
+#%%
+import pandas as pd
+import numpy as np
+from scipy import linalg, sparse # Library for linear algebra
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#%% [markdown]
+### Introduction
+#### Age or stage-sctructure models
