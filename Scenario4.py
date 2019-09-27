0eed6d4ceb56ea3a12aea83c3602162a17891640
VioletteSilvette <55788440+VioletteSilvette@users.noreply.github.com>
2019-09-27 16:09:57 +0200

    Update Scenario4.py

--- Scenario4.py
+++ Scenario4.py
@@ -7,9 +7,10 @@
 
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 def DD_FUNCTION_4(ALPHA,BETA,F_DI,Ei,n):
-    np.multiply(Ei,F_DI)*ALPHA*np.exp(-BETA*n)
+    return(np.multiply(Ei,F_DI)*ALPHA*np.exp(-BETA*n))
 
 def POP_DYNAMICS_4(E) :
     E_resident = E[0]
@@ -23,8 +24,8 @@ def POP_DYNAMICS_4(E) :
     Invader_matrix=np.zeros(shape=(2,2))
     Resident_matrix[0,]= np.multiply(E_resident,F_DI)
     Invader_matrix[0,]= np.multiply(E_invader,F_DI)
-    Resident_matrix[1,] = np.multiply((1-E_resident^z),S_DI) 
-    Invader_matrix[1,] = np.multiply((1-E_invader^z),S_DI)
+    Resident_matrix[1,] = np.multiply((1-E_resident**z),S_DI) 
+    Invader_matrix[1,] = np.multiply((1-E_invader**z),S_DI)
     
     # Run generations with resident only
     Maxgen = 20 # Nbs of generations
@@ -51,12 +52,23 @@ def POP_DYNAMICS_4(E) :
             n_invader = np.dot(Invader_matrix,n_invader)
             Pop_invader[Igen] = sum(n_invader) # Store pop size of invader
             
-    Generation = np.linspace(20,Maxgen,Maxgen-20) # Create vector of generations
+    Generation = np.linspace(21,Maxgen,Maxgen-20) # Create vector of generations
     # Get growth of invader starting at generation 20
-    Pop=n_invader[20:]
+    Pop=Pop_invader[20:]
     Invasion_model=LinearRegression()
-    Invasion_model.fit(Generation.reshape(-1.1),np.log(Pop))
+    Invasion_model.fit(Generation.reshape(-1,1),np.log(Pop))
     #Invasion_model = lm(log(Pop_invader[20:Maxgen])~Generation[20:Maxgen])
     # Elasticity = slope of regression
     Elasticity = float(Invasion_model.coef_)
     return(Elasticity)
+    
+####### MAIN PROGRAM ########
+N1 = 30 # Nos of increments
+E_Resident = np.linspace(0.2,0.9,N1)
+E_Invader = E_Resident.copy()
+d = np.array(np.meshgrid(E_Resident, E_Invader)).reshape(2,len(E_Resident)*len(E_Resident)).T # Equivalent to R's expand.grid()
+z = np.apply_along_axis(POP_DYNAMICS_4, 1, d) 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.contour(E_Resident, E_Invader, z.reshape((N1,N1)), color="black")
+plt.show()
