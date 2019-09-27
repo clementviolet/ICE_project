97b65e686fc0987424f6b5f4a98d6f96d691fdb9
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-09-27 08:49:50 +0200

    Starting scenario 2

--- script.py
+++ script.py
@@ -8,6 +8,7 @@
 import pandas as pd
 import numpy as np
 from scipy import linalg, sparse, optimize # Library for linear algebra
+from sklearn.linear_model import LinearRegression # Linear regression package
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -174,24 +175,30 @@ print(optimize.minimize_scalar(RCALC, bounds=[0.1, 3]).x) # There is a small
 # If the fitness function is too complex, we can approach r numericaly
 
 #%%
-# def SUMMATION(r,x):
-#     Maxage = 50 # Maximum age
-#     age = np.arange(1, Maxage+1)
-#     # Parameter value
-#     Af = 0
-#     Bf = 16
-#     As = 1
-#     Bs = 0.5
-#     m = np.repeat(Af+Bf*x, repeats=Maxage) # Number of female offspring
-#     l = np.exp(-(As+Bs*x)*age) # Surival to age
-#     Sum = sum(np.exp(-r*age)*l*m) #Characteristic equation sum
-#     return(1-Sum)
+def SUMMATION(r,x):
+    Maxage = 50 # Maximum age
+    age = np.arange(1, Maxage+1)
+    # Parameter value
+    Af = 0
+    Bf = 16
+    As = 1
+    Bs = 0.5
+    m = np.repeat(Af+Bf*x, repeats=Maxage) # Number of female offspring
+    l = np.exp(-(As+Bs*x)*age) # Surival to age
+    Sum = sum(np.exp(-r*age)*l*m) #Characteristic equation sum
+    return(1-Sum)
 
 # # Define function to find r given x
-# def RCALC(x):
-#     return(-optimize.brentq(SUMMATION, -1, 2, args=(x)))
+def RCALC(x):
+    return(-1*optimize.brentq(SUMMATION, -1, 2, args=(x))) # -1 to find the minimum of the function
+# - 1 is mandatory, if missing it crash. See RCALC2 for more info
 
-# optimize.minimize_scalar(RCALC, bounds=[0.1, 3])
+# def RCALC2(x):
+    # return(optimize.brentq(SUMMATION, -1, 2, args=(x))) # -1 to find the minimum of the function
+
+
+optimize.minimize_scalar(RCALC, bracket=[0.1,3], method='Golden') # bracket = interval; method='Golden' is the method used by R stats::optimize()
+# optimize.minimize_scalar(RCALC2, bracket=[0.1,3], method='Golden') # bracket = interval; method='Golden' is the method used by R stats::optimize()
 
 # Completer ne fonctionne pas
 
@@ -222,7 +229,7 @@ def RCALC(x):
     Eigen_data = linalg.eig(Leslie_matrix) # Call for eigenvalues
     Lambda = Eigen_data[0][1] # Get lambda
     r = np.abs(np.log(Lambda))
-    return(-r) # Maxium of a functi
+    return(-r) # Maxium of a function is given by the minimum of a -f(x)
 
 Optimum = optimize.minimize_scalar(RCALC, bracket=[1,3], method='Golden') # bracket = interval; method='Golden' is the method used by R stats::optimize()
 Best_x = Optimum.x
@@ -247,4 +254,96 @@ ax.plot(x, r_est, color="black")
 ax.plot(Best_x, Best_r, color="Black", marker="o", markerfacecolor="None")
 ax.set(xlabel="Body size, x", ylabel="r estimated")
 
+plt.show()
+
+#%%[markdown]
+##Scenario 2: Adding density dependence
+###Solving using $R_0$ as the fitness measure
+# Can't do it right now, because I don't understand how optimize.minimize_scalar() works.
+
+###Pairwise invasibility analysis
+
+def LESLIE(x, Maxage):
+    """Function to generate the Leslie's matrix"""
+    M = Maxage-1
+    age = np.arange(1, Maxage+1)
+    # Parameter values
+    Af = 0
+    Bf = 16
+    As = 1
+    Bs = 0.5
+    m = np.repeat(Af+Bf*x, repeats=Maxage)
+    l = np.exp(-(As+Bs*x)*age)
+    S = np.array([l[0]])
+    for i in range(1,M):
+        S = np.append(S, l[i]/l[i-1])
+    S = np.append(S, 0) # Last value is a zero for the last age class
+    Fertility = m*S # Top row of Leslie matrix
+    Survival = np.diag(S[0:M]) # Assign survivals to diagonal
+    Survival = np.insert(Survival, 49, 0, axis=1) # Create the Leslie Matrix without the fertiity
+    # 0 == index where to place the Fertility values, axis=0 place the Fertility values rowise
+    Leslie_matrix = np.insert(Survival, 0, Fertility, axis = 0)# Create our Leslie Matrix
+    return(Leslie_matrix)
+
+def DD(alpha, beta, fi, n):
+    """Densitydependence function"""
+    return(fi*alpha*np.exp(-beta*n))
+
+def POP_DYNAMICS(x):
+    """Population dynamics function"""
+    #x = d[0,:]
+    x_resident = x[0] # Body size of resident population
+    x_invader = x[1] # Body size of invader
+    # Density  dependence parameters
+    alpha = 1
+    beta = 2*10**-5
+    Maxage = 50
+    Resident_matrix = LESLIE(x_resident, Maxage) # Resident Leslie matrix
+    Invader_matrix = LESLIE(x_invader, Maxage) # Invader leslie matrix
+    F_resident = Resident_matrix[0,:].copy() # Resident fertility
+    F_invader = Invader_matrix[0,:].copy() # Invader fertility
+    Maxgen = 30 # Number of gen to run the simulation
+    n_resident = np.zeros(shape=(Maxage,1)) # Resident population vector
+    n_resident[0] = 1 #initial resident pop size == one
+    for Igen in range(1,Maxgen): # Iterate over generations
+        N = sum(n_resident) # Total pop size
+        #Density Dependent fertility of resident for population at time = Igen
+        Resident_matrix[0,:] = DD(alpha, beta, F_resident,N)
+        n_resident = np.dot(Resident_matrix, n_resident)
+    # Introduce invaders
+    Maxgen = 100 # Number of generations to run the simulation
+    n_invader = np.zeros(shape=(Maxage,1))
+    n_invader[0] = 1 # Initial number of invader
+    Pop_invader = np.array([n_invader[0]]).copy() # Store the total population of invader each generation
+    for Igen in range(1, Maxgen):
+        N = sum(n_resident) + sum(n_invader)
+        # Density dependent fertility of resident
+        Resident_matrix[0,:] = DD(alpha, beta, F_resident, N)
+        # Storing the number of new residents
+        n_resident = np.dot(Resident_matrix, n_resident)
+        # Density dependent fertility of invaders
+        Invader_matrix[0,:] = DD(alpha, beta, F_invader, N)
+        # Storing the number of new invaders
+        n_invader = np.dot(Invader_matrix, n_invader)
+        Pop_invader = np.append(Pop_invader, sum(n_invader))
+    # Now do linear regression of log(Pop.invader) on Generation
+    Generation = np.arange(1, Maxgen+1)
+    Nstart = 19 # Number of generations to ignore (i.e 20 first)
+    # Linera regression
+    Invasion_model = LinearRegression()
+    Invasion_model.fit(np.log(Pop_invader[Nstart:Maxgen].reshape((-1,1))), Generation[Nstart:Maxgen])
+    Elasticity = Invasion_model.coef_
+    return(Elasticity)
+
+N1 = 30 # Number of increment
+X_Resident = np.linspace(1,3,N1)
+X_Invader = X_Resident.copy()
+d = np.array(np.meshgrid(X_Resident, X_Invader)).reshape(2,len(X_Resident)*len(X_Resident)).T # Equivalent to R's expand.grid()
+z = np.apply_along_axis(POP_DYNAMICS, 1, d) 
+# Problème d'arrondis....
+z1 = z[0:450]
+z2 = z[451:900]
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.contour(X_Resident, X_Invader, z.reshape((30,30)), color="black")
 plt.show()
